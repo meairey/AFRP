04d0c0e4fa61de6c737043a4065dae5603ac9a11
Montana Airey <ma2276@cornell.edu>
2021-11-04 21:55:57 -0400

    graphs

--- Montana's Code/Community_Data.R
+++ Montana's Code/Community_Data.R
@@ -13,7 +13,7 @@ sample = read.csv("Data/2021_Sample.csv")
 # Join/Filter ---------
 all_data = left_join(fish, sample, by = "YSAMP_N") %>% 
    
-  filter(WATER == "LOP",
+  filter(WATER == "HRM",
          ### single out or remove specific species
          (SPECIES != "NF" & SPECIES != ""),
          SPECIES != "PAINTED TURTLE",
@@ -25,9 +25,9 @@ all_data = left_join(fish, sample, by = "YSAMP_N") %>%
          SPECIES != "LEECH", 
          SPECIES != "MOLE SALAMANDER LARVAE", 
          SPECIES != "ODONATA") %>%
-  select(YSAMP_N, SPECIES, LENGTH, DATE_COL) %>%
+  select(YSAMP_N, SPECIES, LENGTH, DATE_COL, SITE_N) %>%
   separate(DATE_COL, into = c("MONTH","DAY","YEAR"), sep = "/") %>%
-  filter(MONTH ==8)
+  filter(MONTH ==9)
 
 TPP = all_data %>%
   group_by(SPECIES, MONTH) %>%
@@ -69,7 +69,6 @@ PRL %>% ggplot(aes(x = SPECIES, y = n)) +
 
 
 all_data = left_join(fish, sample, by = "YSAMP_N") %>% 
-  
   filter(### single out or remove specific species
          (SPECIES != "NF" & SPECIES != ""),
          SPECIES != "PAINTED TURTLE",
@@ -84,7 +83,10 @@ all_data = left_join(fish, sample, by = "YSAMP_N") %>%
          WATER != "COM") %>%
   select(YSAMP_N, SPECIES, LENGTH, DATE_COL, WATER) %>%
   separate(DATE_COL, into = c("MONTH","DAY","YEAR"), sep = "/") %>%
-  filter(MONTH ==8)
+  filter(MONTH ==8) %>% as.data.frame() %>%
+  complete(SPECIES, WATER) %>% 
+  replace_na(list(CPUE_seconds = 0, n = 0))
+  
 
 all = all_data %>%
   group_by(SPECIES,WATER) %>%
@@ -93,6 +95,39 @@ all %>% ggplot(aes(x = SPECIES, y = n, fill = WATER)) +
   geom_bar(stat="identity", position=position_dodge()) + 
   theme(axis.text.x=element_text(angle=90,hjust=1),
         text = element_text(size=18)) + 
-  ggtitle("Panther Lake")+ 
+  ggtitle("Summer 21")+ 
   ylab("# Individuals Captured")
 
+all %>% ggplot(aes(x = WATER, y = n, fill = SPECIES)) +
+  geom_bar(stat="identity", position=position_dodge()) + 
+  theme(axis.text.x=element_text(angle=90,hjust=1),
+        text = element_text(size=18)) + 
+  ggtitle("Summer 21")+ 
+  ylab("# Individuals Captured") + 
+  labs(fill = "Species")
+
+
+
+HRM = all_data %>%
+  group_by(SPECIES, SITE_N) %>%
+  count() %>% as.data.frame() %>%
+  complete(SPECIES, SITE_N) %>% 
+  replace_na(list(CPUE_seconds = 0, n = 0))
+
+HRM %>% ggplot(aes(x = SPECIES, y = n, fill = SITE_N)) +
+  geom_bar(stat="identity", position=position_dodge()) + 
+  theme(axis.text.x=element_text(angle=90,hjust=1),
+        text = element_text(size=18)) + 
+  ggtitle("Heron Marsh")+ 
+  ylab("# Individuals Captured") + 
+  labs(fill = 'Site') + 
+  xlab("Species")
+
+HRM %>% ggplot(aes(y = n, x = SITE_N, fill = SPECIES)) +
+  geom_bar(stat="identity", position=position_dodge()) + 
+  theme(axis.text.x=element_text(angle=90,hjust=1),
+        text = element_text(size=18)) + 
+  ggtitle("Heron Marsh")+ 
+  ylab("# Individuals Captured") + 
+  labs(fill = 'Site') + 
+  xlab("Species")
