50d46bfc9372b904b1c70d4fb2c30228b195d119
Montana Airey <ma2276@cornell.edu>
2021-12-06 19:11:18 -0500

    updating IDs

--- Montana's Code/Isotopes/Relative_Area.R
+++ Montana's Code/Isotopes/Relative_Area.R
@@ -3,8 +3,7 @@ library(easypackages)
 libraries("snow","plotrix", "SIBER","ggplot2", "tidyr","ellipse","mixtools",
           "mvtnorm","plot3D","scatterplot3d","scales","viridis","ggplot2",
           "gridExtra", "dplyr","RColorBrewer")
-setwd("C:/Users/monta/OneDrive - Airey Family/GitHub/AFRP/Montana's Code")
-source("isotope_functions.R")
+source("Isotopes/isotope_functions.R")
 ## Run Siber ------------------- 
 # Do I need this?
 #siber.example <- createSiberObject(x) 
@@ -14,7 +13,7 @@ source("isotope_functions.R")
 
 ## Proportion of axes lakes -----------------
 for(h in 3){
-  d = data_setup(x,h,11)
+  d = data_setup(data,h,11)
   names = legend$color[sort(unique(x$group))]
   spp=length(names(d[[2]]))
   spec = names(d[[2]]) %>%
@@ -278,4 +277,134 @@ for(h in 2:4){
 }
 
 
+## Proportion of axes - different -------------------
+
+for(h in 3){
+  h = 3
+  dat = data_setup(data,h,"PD") ## Set up the data 
+  ellipse.area = siberEllipses(dat[[2]]) # Run SIBER to get ellipse area
+  TA = rowSums(ellipse.area) ## Get Total Area
+  ellipse.area = ellipse.area/TA # Get total area of each ellipse
+  names = unique(legend$Species[dat[[3]]$group])
+  colnames(ellipse.area) = names
+  vec = vector()
+  ellip = array(0, dim=c(n.posts, 4, length(names)))
+  blank_vec_C = rep(0, n.posts)
+  blank_vec_N = rep(0, n.posts)
+  ellip = ellip_data(length(names), n.posts, dat)
+  vector = rep(names, each = n.posts)
+  spp.c = vector()
+  spp.n = vector()
+  spp.post.n = vector()
+  spp.post.c = vector()
+  for(j in 1:n.posts){ # Don't run this multiple times it messes the rbind
+    C_range = max(ellip[j,3,])- min(ellip[j,1,])
+    N_range = max(ellip[j,4,]) - min(ellip[j,2,])
+    for(i in 1:(length(names))){
+      spp.post.c[i] = (max(ellip[j,3,i])- min(ellip[j,1,i]))/C_range
+      spp.post.n[i] = (max(ellip[j,4,i])- min(ellip[j,2,i]))/N_range
+    }
+    spp.c = rbind(spp.c, spp.post.c)
+    spp.n = rbind(spp.n, spp.post.n)
+  }
+  for(i in 1:length(spp.c[1,])){
+    SPP.C= spp.c[spp.c[,i] >  quantile(x = spp.c[,i] ,probs = .05) &
+                   spp.c[,i] < quantile(x = spp.c[,i] ,probs = .95),i]
+    
+    SPP.C = c(SPP.C, rep("NA", (1000 - length(SPP.C)))) %>% as.numeric()
+    
+    blank_vec_C = cbind(blank_vec_C, SPP.C)
+    
+    SPP.N= spp.n[spp.n[,i] >  quantile(x = spp.n[,i] ,probs = .05) &
+                   spp.n[,i] < quantile(x = spp.n[,i] ,probs = .95),i]
+    SPP.N = c(SPP.N, rep("NA", (1000 - length(SPP.N)))) %>% as.numeric()
+    
+    blank_vec_N = cbind(blank_vec_N, SPP.N, dat[[2]][[i]][1:n.posts,6])
+  }
+  
+  for(i in 1:length(spp.c[1,])){
+    
+    
+    blank_vec_C = cbind(blank_vec_C, dat[[2]][[i]][1:n.posts,5])
+    
+    blank_vec_N = cbind(blank_vec_C, dat[[2]][[i]][1:n.posts,6])
+    
+  }
+  
+  
+  C_data = blank_vec_C[,-1]
+  N_data = blank_vec_N[,-1]
+  
+  col_ID = data.frame(sp = rep(names, 2)) %>% 
+    mutate(type = rep(c("proportion", "mean"), 
+                      each = length(names))) %>% 
+    unite("ID",c(sp, type))
+  
+  
+  
+  colnames(C_data)= col_ID$ID
+  
+  median_C = C_data[, 1:length(names)] %>% as.data.frame()%>%
+    summarise_all(list(median), na.rm = T)
+  
+  colnames(N_data)=names
+  
+  median_N = N_data %>% as.data.frame() %>%
+    summarise_all(list(median), na.rm = T)
+  
+  
+  frame_C = C_data %>% as.data.frame() %>%
+    pivot_longer(1:length(C_data[1,]),
+                 names_to = "SP",
+                 values_to = "Dat_C") 
+  p = ggplot() + 
+    geom_point(frame_C, mapping=aes(y = SP, x = Dat_C, col = SP)) +
+    xlim(0,1) + ggtitle(paste(lake$Water[h])) +
+    ylab("Species") + 
+    xlab("Proportion of C axis")+
+    theme(text = element_text(size = 18)) +
+    geom_point(mapping =aes(x = as.numeric(median_C[1,]), 
+                            y = names),
+               col = unique(legend$color[sort(unique(dat[[3]]$group))]),
+               pch = "|", size = 4) + 
+    scale_color_manual(values = legend$color[sort(unique(dat[[3]]$group))], 
+                       
+                       name = "Species")
+  print(p)
+  colnames(N_data)=names
+  frame_N = N_data %>% as.data.frame() %>%
+    pivot_longer(1:length(C_data[1,]),
+                 names_to = "SP_N",
+                 values_to = "Dat_N")   
+  g = ggplot() + 
+    geom_point(frame_N, mapping=aes(y = SP_N, x = Dat_N, col = SP_N)) + 
+    geom_point()+ geom_point() +
+    xlim(0,1) + ggtitle(paste(lake$Water[h])) +
+    ylab("Species") + 
+    xlab("Proportion of N axis")+
+    theme(text = element_text(size = 18))+
+    geom_point(mapping =aes(x = as.numeric(median_N[1,]), 
+                            y = names),
+               col = unique(legend$color[sort(unique(dat[[3]]$group))]),
+               pch = "|", size = 4) + 
+    scale_color_manual(values = legend$color[sort(unique(dat[[3]]$group))], 
+                       
+                       name = "Species")
+  
+  print(g)
+
+  
+  dat[[2]][[i]][j,6])
+  
+  
+  
+  l = ggplot() +  
+  
+  
+  
+  
+  
+
+  
+}
 
